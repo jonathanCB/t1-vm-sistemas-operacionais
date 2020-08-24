4790d7eaae8c50c0ba7e1dbb189f4b541c57e7ea
Jonathan <jonas.ca2@gmail.com>
2020-08-24 20:09:32 -0300

    Pequena alterção no App.java

--- VM/App.java
+++ VM/App.java
@@ -4,12 +4,12 @@ public class App {
 	public static void main(String args[]) {
 		VM vm = new VM();
 		VM vm1 = new VM();
-		VM vm2 = new VM();
-		/*VM vm3 = new VM();
-		VM vm4 = new VM();*/
+		//VM vm2 = new VM();
+		VM vm3 = new VM();
+		//VM vm4 = new VM();
 		vm.testeProgramas();
 		vm1.p1Fibonacci();
-		vm2.p3Fatorial();
+		vm3.p3Fatorial();
 
 	}
 }
